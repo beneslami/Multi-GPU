df3bf0e0bad1ff7b58c719bb0010547a12784d63
Benyamin <beneslami72@gmail.com>
2021-01-10 13:33:49 +0100

    export data to a file

--- src/gpgpu-sim/RateCounter.cc
+++ src/gpgpu-sim/RateCounter.cc
@@ -5,12 +5,15 @@ a class which counts the number of occurrence of a remote request per second.
 
 #include "RateCounter.h"
 #include <iostream>
+#include <fstream>
 
 RateCount::RateCount(time_t period) :
                                     m_lastFlush(std::time(NULL)),
                                     m_period(period),
                                     m_count(0)
-{}
+{
+    std::ofstream file("../../remote.txt", file.out | file.app);
+}
 
 void RateCount::count()
 {
@@ -18,10 +21,12 @@ void RateCount::count()
     time_t now = std::time(NULL);
     if((now - m_lastFlush) >= m_period)
     {
+        file.open("../../remote.txt", std::ios::app);
         size_t count_per_sec = 0.0;
         if(m_count > 0)
         {
             count_per_sec = m_count / (now - m_lastFlush);
+            file << count_per_sec << "-->" << now;
         }
         std::cout << count_per_sec << " remote access per second" << std::endl;
         m_count = 0;
--- src/gpgpu-sim/RateCounter.h
+++ src/gpgpu-sim/RateCounter.h
@@ -7,6 +7,7 @@ a class which counts the number of occurrence of a remote request per second.
 #define RATECOUNTER_H
 
 #include <ctime>
+#include <fstream>
 
 class RateCount
 {
@@ -14,7 +15,7 @@ protected:
     time_t m_lastFlush;
     time_t m_period;
     time_t m_count;
-
+    std::ofstream file;
 public:
     RateCount(time_t period);   // Constructor
     //~RateCount();               // Distructor 
