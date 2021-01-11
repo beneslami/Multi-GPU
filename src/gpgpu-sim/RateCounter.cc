19cdd2a25e8f18e79aa78aa6171a8077d3b704fa
Benyamin <beneslami72@gmail.com>
2021-01-11 13:57:11 +0100

    bug fix

--- src/gpgpu-sim/RateCounter.cc
+++ src/gpgpu-sim/RateCounter.cc
@@ -27,7 +27,7 @@ void RateCount::count()
         if(m_count > 0)
         {
             count_per_sec = m_count / (now - m_lastFlush);
-            file << count_per_sec << " --> " << ltm->tm_mday << " " << ltm->tm_hour << ":" << ltm->tm_min << ":" << ltm->tm_sec << endl;
+            file << count_per_sec << " --> " << ltm->tm_mday << " " << ltm->tm_hour << ":" << ltm->tm_min << ":" << ltm->tm_sec << std::endl;
         }
         std::cout << count_per_sec << " remote access per second" << std::endl;
         m_count = 0;
