0f9813a62bfcef093fc30d7a3f862b979b065f0d
Benyamin <beneslami72@gmail.com>
2021-01-18 19:33:57 +0100

    add

--- src/gpgpu-sim/RateCounter.cc
+++ src/gpgpu-sim/RateCounter.cc
@@ -36,3 +36,61 @@ void RateCount::count()
         file.close();
     }
 }
+
+/*
+ #include <iostream>
+#include <ctime>
+#include <sys/time.h>
+#include <iomanip>
+//#include <bits/stdc++.h>
+
+class RateCount
+{
+protected:
+    size_t m_count;
+    clock_t m_lastFlush;
+    size_t m_period;
+
+public:
+    RateCount(size_t period);   // Constructor
+    //~RateCount();               // Distructor
+    void count();
+};
+
+RateCount::RateCount(size_t period){
+  m_lastFlush = clock();
+  m_period = (period / double(CLOCKS_PER_SEC))*(1000000);
+  m_count = 0;
+}
+
+void RateCount::count(){
+  m_count++;
+  clock_t now = clock();
+  double time_slot_microsecond = ((now - m_lastFlush) / double(CLOCKS_PER_SEC))*(1000000); //
+
+  if(time_slot_microsecond >= m_period){
+    size_t count_rate = 0.0;
+    if(m_count > 0){
+        count_rate = m_count / m_period;
+        std::cout << "first: " <<  m_lastFlush / double(CLOCKS_PER_SEC) << std::setprecision(10) << std::endl;
+        std::cout << "last: " << now / double(CLOCKS_PER_SEC) << std::setprecision(10) << std::endl;
+        std::cout << time_slot_microsecond << " : " << count_rate << std::endl;
+        std::cout <<"----------" << std::endl;
+    }
+    //cout << time_slot_milisecond/std::chrono::milliseconds(1);
+    m_count = 0;
+    m_lastFlush = now;
+  }
+}
+
+int main()
+{
+  RateCount rate_count = RateCount(1);
+  for(long int i = 0; i < 1000000000; i++){
+    if(i %4 == 0 ){
+      rate_count.count();
+    }
+  }
+  return 0;
+}
+ */
