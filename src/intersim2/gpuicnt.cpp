5e0f49762304c099f6c519db001a2c99df1be869
Benyamin <beneslami72@gmail.com>
2021-01-31 20:40:36 +0100

    bug fixes

--- src/intersim2/gpuicnt.cpp
+++ src/intersim2/gpuicnt.cpp
@@ -9,16 +9,16 @@ std::fstream file;
 
 InterGPU::InterGPU() {
     file.open("remote.txt", std::ios::app);
-    file << "\tinput\t" << "output\t" << "size\t" << "Packet Type\t" <<"chip ID\t" << "sub_part ID\t" << "read/write\t" << "cycle(s)\t" << "time\n";
+    file << "\tinput\t" << "output\t" << "size\t" << "Packet_Type\t" <<"chip_ID\t" << "sub_part_ID\t" << "read/write\t" << "cycle(s)\t" << "time\n";
     file.close();
 }
 
-void InterGPU::apply(const char *func, unsigned input_deviceID, unsigned output_deviceID, int ptype, unsigned int size, int chip_id, unsigned int sub_partition_id, const char* is_write) {
+void InterGPU::apply(const char *func, unsigned input_deviceID, unsigned output_deviceID, unsigned int size, int ptype, int chip_id, unsigned int sub_partition_id, const char* is_write) {
     file.open("remote.txt", std::ios::app);
     time_t ttime = time(0);
     tm *current = localtime(&ttime);
     if(file.is_open()){
-        file << func << "\t" << input_deviceID << "\t" << output_deviceID << "\t" << size << "\t" << ptype << "\t" << chip_id << "\t" << sub_partition_id << "\t\t" << is_write << "\t\t" << this->end - this->start << "\t" << current->tm_min << ":" << current->tm_sec << std::endl;
+        file << func << "\t" << input_deviceID << "\t" << output_deviceID << "\t" << size << "\t" << ptype << "\t\t" << chip_id << "\t\t" << sub_partition_id << "\t\t" << is_write << "\t\t" << this->end - this->start << "\t" << current->tm_min << ":" << current->tm_sec << std::endl;
     }
     file.close();
 }
