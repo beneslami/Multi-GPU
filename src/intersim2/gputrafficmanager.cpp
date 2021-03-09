52506e3e09a88726232a155848a66ab55db22dda
Benyamin <beneslami72@gmail.com>
2021-03-09 22:45:42 +0100

    monitor

--- src/intersim2/gputrafficmanager.cpp
+++ src/intersim2/gputrafficmanager.cpp
@@ -28,7 +28,7 @@
 #include <sstream>
 #include <fstream>
 #include <limits> 
-
+#include <iostream>
 #include "gputrafficmanager.hpp"
 #include "interconnect_interface.hpp"
 #include "globals.hpp"
@@ -335,7 +335,7 @@ void GPUTrafficManager::_Step()
         cout << "WARNING: Possible network deadlock.\n";
     }
     vector<map<int, Flit *> > flits(_subnets);
-    fprintf(stdout, "subnets: %d\tnodes: %n", _subnets, _nodes);
+    std::cout << "subnets: " << _subnets << "nodes: " << _nodes << endl;
     for ( int subnet = 0; subnet < _subnets; ++subnet ) {
         for ( int n = 0; n < _nodes; ++n ) {
             Flit * const f = _net[subnet]->ReadFlit( n );
