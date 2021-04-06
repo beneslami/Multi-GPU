74b85792086aae673b6f0d5d3d9efa943d4173b5
Benyamin <beneslami72@gmail.com>
2021-04-06 20:45:03 +0200

    class bug fix

--- src/intersim2/interconnect_interface.cpp
+++ src/intersim2/interconnect_interface.cpp
@@ -155,7 +155,7 @@ void InterconnectInterface::Init() {
 
 void InterconnectInterface::Push(unsigned input_deviceID, unsigned output_deviceID, void *data, unsigned int size) {
 // it should have free buffer
-    //assert(HasBuffer(input_deviceID, size));
+    assert(HasBuffer(input_deviceID, size));
     int output_icntID = _node_map[output_deviceID];
     int input_icntID = _node_map[input_deviceID];
 // n_shader: 128, n_mem: 64, n_node: 196
@@ -244,7 +244,7 @@ void InterconnectInterface::Push(unsigned input_deviceID, unsigned output_device
     }
     //assert(HasBuffer_new(input_deviceID, size, subnet, cl));
 //TODO: _include_queuing ?
-    _traffic_manager->_GeneratePacket(input_icntID, -1, cl /*class*/, _traffic_manager->_time, subnet, n_flits,
+    _traffic_manager->_GeneratePacket(input_icntID, -1, 0 /*class*/, _traffic_manager->_time, subnet, n_flits,
                                       packet_type, data, output_icntID);
 
 #if DOUB
