f5c866a4cd04ebe9a1634fb98fb87232d8ad28d6
Benyamin <beneslami72@gmail.com>
2021-04-06 14:02:20 +0200

    class setup

--- src/intersim2/booksim_config.cpp
+++ src/intersim2/booksim_config.cpp
@@ -152,7 +152,7 @@ BookSimConfig::BookSimConfig( )
   
   //==== Traffic ========================================
 
-  _int_map["classes"] = 1;
+  _int_map["classes"] = 4;
 
   AddStrField( "traffic", "uniform" );
 
