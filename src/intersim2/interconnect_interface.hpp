1e9a0bc03cdfe4c76ff8c003aed2cccca76c1b2b
Benyamin <beneslami72@gmail.com>
2021-04-06 15:59:59 +0200

    core dump fix

--- src/intersim2/interconnect_interface.hpp
+++ src/intersim2/interconnect_interface.hpp
@@ -59,7 +59,7 @@ public:
   virtual void Advance();
   virtual bool Busy() const;
   virtual bool HasBuffer(unsigned deviceID, unsigned int size) const;
-  bool HasBuffer_new(unsigned deviceID, unsigned int size, int sub, int cl)
+  bool HasBuffer_new(unsigned deviceID, unsigned int size, int sub, int cl);
   virtual void DisplayStats() const;
   virtual void DisplayOverallStats() const;
   unsigned GetFlitSize() const;
