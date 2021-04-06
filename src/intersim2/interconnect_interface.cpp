72e8dd6b5b8a92166802e8a0bc50ca2cd3d941a5
Benyamin <beneslami72@gmail.com>
2021-04-06 15:28:02 +0200

    core dump fix

--- src/intersim2/interconnect_interface.cpp
+++ src/intersim2/interconnect_interface.cpp
@@ -142,12 +142,11 @@ void InterconnectInterface::Init() {
 
 void InterconnectInterface::Push(unsigned input_deviceID, unsigned output_deviceID, void *data, unsigned int size) {
 // it should have free buffer
-
+    assert(HasBuffer(input_deviceID, size));
     int output_icntID = _node_map[output_deviceID];
     int input_icntID = _node_map[input_deviceID];
 // n_shader: 128, n_mem: 64, n_node: 196
-#if
-    0
+#if 0
     cout << "Call interconnect push input: " << input << " output: " << output << endl;
 #endif
 
@@ -230,13 +229,11 @@ void InterconnectInterface::Push(unsigned input_deviceID, unsigned output_device
             cl = 0;
         }
     }
-    assert(HasBuffer(input_deviceID, size, subnet, cl));
 //TODO: _include_queuing ?
     _traffic_manager->_GeneratePacket(input_icntID, -1, cl /*class*/, _traffic_manager->_time, subnet, n_flits,
                                       packet_type, data, output_icntID);
 
-#if
-    DOUB
+#if DOUB
     cout << "Traffic[" << subnet << "] (mapped) sending form " << input_icntID << " to " << output_icntID << endl;
 #endif
 //  }
@@ -245,8 +242,7 @@ void InterconnectInterface::Push(unsigned input_deviceID, unsigned output_device
 void *InterconnectInterface::Pop(unsigned deviceID) {
     int icntID = _node_map[deviceID];
     int v;
-#if
-    DEBUG
+#if DEBUG
     cout << "Call interconnect POP  " << output << endl;
 #endif
     void *data = NULL;
