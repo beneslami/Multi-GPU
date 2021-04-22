1346d0fa4d91f6e9063c2799edd65708e1bb9a10
Benyamin <beneslami72@gmail.com>
2021-04-22 15:33:34 +0200

    L1D cache trace

--- src/gpgpu-sim/gpu-sim.cc
+++ src/gpgpu-sim/gpu-sim.cc
@@ -2612,7 +2612,6 @@ void gpgpu_sim::cycle() {
                     }
                 }
             }
-
         }
 /*
         for(int i = 0; i < Drain_list.size(); i++) 
@@ -2842,18 +2841,18 @@ kain comment end*/
                 } else if (mf != NULL && INTER_TOPO == 1) { //ZSQ0126, 1 for ring, forwarding if not neighbor
                     unsigned _mid = mf->get_chip_id();
                     unsigned _subid = mf->get_sub_partition_id();
-                if (mf->get_type() == READ_REPLY || mf->get_type() == WRITE_ACK) { //reply
-                    if (i == mf->get_sid()/32 && !KAIN_NoC_r.inter_icnt_pop_llc_full(_subid)) //arrive
-                    KAIN_NoC_r.inter_icnt_pop_llc_push(mf, _subid);
-                    else if (i != mf->get_sid()/32 && !KAIN_NoC_r.forward_waiting_full(i))//forward
-                    KAIN_NoC_r.forward_waiting_push(mf, i);
-                }
-                else { //request
-                    if (i == mf->get_chip_id()/8 && !KAIN_NoC_r.inter_icnt_pop_mem_full(_mid)) //arrive
-                                KAIN_NoC_r.inter_icnt_pop_mem_push(mf, _mid);
-                            else if (i != mf->get_chip_id()/8 && !KAIN_NoC_r.forward_waiting_full(i))//forward
-                                KAIN_NoC_r.forward_waiting_push(mf, i);
-                }
+                    if (mf->get_type() == READ_REPLY || mf->get_type() == WRITE_ACK) { //reply
+                        if (i == mf->get_sid()/32 && !KAIN_NoC_r.inter_icnt_pop_llc_full(_subid)) //arrive
+                        KAIN_NoC_r.inter_icnt_pop_llc_push(mf, _subid);
+                        else if (i != mf->get_sid()/32 && !KAIN_NoC_r.forward_waiting_full(i))//forward
+                        KAIN_NoC_r.forward_waiting_push(mf, i);
+                    }
+                    else { //request
+                        if (i == mf->get_chip_id()/8 && !KAIN_NoC_r.inter_icnt_pop_mem_full(_mid)) //arrive
+                                    KAIN_NoC_r.inter_icnt_pop_mem_push(mf, _mid);
+                                else if (i != mf->get_chip_id()/8 && !KAIN_NoC_r.forward_waiting_full(i))//forward
+                                    KAIN_NoC_r.forward_waiting_push(mf, i);
+                    }
                 }
             }
         //	printf("ZSQ: leave SM_SIDE_LLC == 1 C\n");
