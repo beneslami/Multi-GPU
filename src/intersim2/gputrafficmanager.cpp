0db5adcb57cda2761467d8b6b7a1e19dbb1f32a8
Ben <beneslami72@gmail.com>
2021-09-10 13:25:09 +0200

    bug fix

--- src/intersim2/gputrafficmanager.cpp
+++ src/intersim2/gputrafficmanager.cpp
@@ -34,6 +34,8 @@
 #include "globals.hpp"
 
 extern unsigned long long gpu_sim_cycle;
+extern unsigned long long gpu_tot_sim_cycle;
+
 GPUTrafficManager::GPUTrafficManager( const Configuration &config, const vector<Network *> &net)
 :TrafficManager(config, net)
 {
@@ -274,7 +276,7 @@ void GPUTrafficManager::_GeneratePacket(int source, int stype, int cl, int time,
     f->watch  = watch | (gWatchOut && (_flits_to_watch.count(f->id) > 0));
     f->subnetwork = subnetwork;
     f->src    = source;
-    f->ctime  = gpu_sim_cycle;
+    f->ctime  = gpu_tot_sim_cycle;
     f->record = record;
     f->cl     = cl;
     f->data = data;
@@ -608,7 +610,7 @@ void GPUTrafficManager::_Step()
           << "." << endl;
         }
         //f->itime = _time;
-        f->itime = gpu_sim_cycle;
+        f->itime = gpu_tot_sim_cycle;
 
         // Pass VC "back"
         if(!_input_queue[subnet][n][c].empty() && !f->tail) {
@@ -641,7 +643,7 @@ void GPUTrafficManager::_Step()
         Flit * const f = iter->second;
 
         //f->atime = _time;
-        f->atime = gpu_sim_cycle;
+        f->atime = gpu_tot_sim_cycle;
 
         if(f->watch) {
           *gWatchOut << GetSimTime() << " | "
