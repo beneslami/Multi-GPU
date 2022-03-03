2d910123ab36b7c90c6ad63b2376304ff362c925
Benyamin <beneslami72@gmail.com>
2022-03-03 13:47:32 +0100

    add output

--- src/gpgpu-sim/shader.cc
+++ src/gpgpu-sim/shader.cc
@@ -4426,7 +4426,7 @@ void simt_core_cluster::icnt_inject_request_packet(class mem_fetch *mf)
                    ::_get_icnt_cycle() << "\tchip: " << mf->get_chiplet() << "\tsize: " << mf->get_ctrl_size()
                    <<"\tgpu_cycle: " << gpu_sim_cycle << "\n";
               rep1->apply(out1.str().c_str());
-              rep1->icnt_apply(out.str().c_str());
+              rep1->icnt_apply(out1.str().c_str());
           }
 #endif
       }
