310ee41e4260a0ac8ade0e0776997b69676ed446
Benyamin <beneslami72@gmail.com>
2022-05-07 13:37:51 +0200

    output print

--- src/gpgpu-sim/l2cache.cc
+++ src/gpgpu-sim/l2cache.cc
@@ -45,7 +45,7 @@
 #include "mem_latency_stat.h"
 #include "l2cache_trace.h"
 //#include "../ramulator_sim/Config.h"
-
+#include <sstream>
 extern unsigned long long rop_in;
 extern unsigned long long rop_out;
 extern unsigned long long icnt_L2_in;
