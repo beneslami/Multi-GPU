b9b1ef4524e04cce75b2497ee73f0bb65bc343f2
ben <beneslami72@gmail.com>
2022-06-24 15:55:57 +0200

    output

--- src/gpgpu-sim/shader.h
+++ src/gpgpu-sim/shader.h
@@ -1550,6 +1550,14 @@ public:
     				       m_core_id, 
     				       m_cluster_id,
     				       m_memory_config );
+        if(mf->get_sid()/32 != mf->get_chip_id()/8){
+            std::ostringstream out;
+            out << mf->get_sid()/32 << " " << mf->get_chip_id()/8 << std::endl;
+            std::fstream outdata;
+            outdata.open("core.txt", std::ios_base::app);
+            outdata << out.str().c_str();
+            outdata.close();
+        }
     	return mf;
     }
     
@@ -1564,12 +1572,16 @@ public:
                                       m_core_id, 
                                       m_cluster_id, 
                                       m_memory_config);
-        std::ostringstream out;
-        out << mf->get_sid()/32 << " " << mf->get_chip_id()/8 << std::endl;
-        std::fstream outdata;
-        outdata.open("core.txt", std::ios_base::app);
-        outdata << out.str().c_str();
-        outdata.close();
+
+        /*if(mf->get_sid()/32 != mf->get_chip_id()/8){
+            std::ostringstream out;
+            out << mf->get_sid()/32 << " " << mf->get_chip_id()/8 << std::endl;
+            std::fstream outdata;
+            outdata.open("core.txt", std::ios_base::app);
+            outdata << out.str().c_str();
+            outdata.close();
+        }*/
+
 #endif
 #if STATISTICS == 1
 
