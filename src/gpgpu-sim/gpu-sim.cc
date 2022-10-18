a6bf3224bb8dcd17e1b299b09ddba6a07a242108
ben <beneslami72@gmail.com>
2022-10-18 14:50:30 +0200

    temporary output

--- src/gpgpu-sim/gpu-sim.cc
+++ src/gpgpu-sim/gpu-sim.cc
@@ -2985,6 +2985,10 @@ void gpgpu_sim::cycle()
                                request_size = mf->size();
                            //m_memory_sub_partition[i]->push( mf, gpu_sim_cycle + gpu_tot_sim_cycle + 32);
                            m_memory_sub_partition[i]->push(mf, gpu_sim_cycle + gpu_tot_sim_cycle);
+                           if(request_size == 136){
+                               cout <<"1-src: " << mf->get_src() << "\tdest: " << mf->get_dst() << "\ttype: " <<
+                               mf->get_type() << "\tsize: " << mf->size() << "\tis write: " << mf->is_write() << "\tID: " << mf->get_request_uid() <<std::endl;
+                           }
                            KAIN_NoC_r.set_inter_icnt_pop_llc_turn(i);
                            if (gpu_sim_cycle >= 1000000) {
                                out << "rop push\tsrc: " << mf->get_src() << "\tdst: " << mf->get_dst() <<
@@ -3033,8 +3037,9 @@ void gpgpu_sim::cycle()
                                request_size = mf->get_ctrl_size();
                            else if (mf->get_type() == READ_REPLY || mf->get_type() == WRITE_REQUEST)
                                request_size = mf->size();
-                           if(request_size != 8){
-                               cout <<"src: " << mf->get_src() << "\tdest: " << mf->get_dst() << "\ttype: " << mf->get_type() << "\tsize: " << mf->size() << "\tis write: " << mf->is_write() << std::endl;
+                           if(request_size == 136){
+                               cout <<"2-src: " << mf->get_src() << "\tdest: " << mf->get_dst() << "\ttype: " <<
+                               mf->get_type() << "\tsize: " << mf->size() << "\tis write: " << mf->is_write() << "\tID: " << mf->get_request_uid() <<std::endl;
                            }
                            if (gpu_sim_cycle >= 1000000) {
                                out << "rop push\tsrc: " << mf->get_src() << "\tdst: " << mf->get_dst() <<
