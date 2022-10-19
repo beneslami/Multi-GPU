83c8e877b5ebb78d95bbb64bf66c82ef64d00db0
ben <beneslami72@gmail.com>
2022-10-19 17:11:03 +0200

    temporary output

--- src/gpgpu-sim/shader.h
+++ src/gpgpu-sim/shader.h
@@ -1565,16 +1565,6 @@ public:
                                       m_cluster_id, 
                                       m_memory_config);
 
-        /*if(mf->get_sid()/32 != mf->get_chip_id()/8){
-            std::ostringstream out;
-            out << "size: " << access.get_size() << "\ttype: " << access.get_type() << std::endl;
-
-            std::fstream outdata;
-            outdata.open("core.txt", std::ios_base::app);
-            outdata << out.str().c_str();
-            outdata.close();
-        }*/
-
 #endif
 #if STATISTICS == 1
 
